91b697621cd85a0146106f592fe94528b8b30506
Walrusking <walruskinggaming@gmail.com>
2020-09-11 19:00:46 -0400

    Style: Updated css

--- src/style/css.ts
+++ src/style/css.ts
@@ -1,22 +1,19 @@
 import {fn} from "../core/core";
 
-// @ts-ignore
-fn.css = (...cssArgs) => {
+fn.prototype.css = (...cssArgs) => {
 	if (typeof cssArgs[0] === 'string') {
 		const [property, value] = cssArgs;
-		// @ts-ignore
-		this.forEach((element) => {
+		fn.prototype.this.forEach((element) => {
 			element.style[property] = value;
 		});
 	} else if (typeof cssArgs[0] === 'object') {
 		// @ts-ignore
 		const cssProps = Object.entries(cssArgs[0]);
-		// @ts-ignore
-		this.forEach((element) => {
+		fn.prototype.this.forEach((element) => {
 			cssProps.forEach(([property, value]) => {
 				element.style[property] = value;
 			});
 		});
 	}
-	return this;
+	return fn.prototype;
 }
