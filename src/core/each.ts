02f3c77704f2bb56db9b3a293d220a281b86d1c3
Walrusking <walruskinggaming@gmail.com>
2020-09-11 18:57:26 -0400

    Revert "Core: Added on"
    This reverts commit c1b78a996b175757e6552e2e714db08033c7bada.

--- src/core/each.ts
+++
@@ -1,9 +0,0 @@
-import {fn} from "./core";
-
-fn.prototype.each = (callback) => {
-	fn.prototype.this.forEach((element, i) => {
-		const boundFn = callback.bind(element);
-		boundFn(element, i);
-	});
-	return fn.prototype;
-}
