741745fa5ffb19e8fc6c65c54743aec8528a4c0a
Walrusking <walruskinggaming@gmail.com>
2020-09-12 04:02:09 -0400

    Elements: Updated children,clone,empty,replace

--- src/elements/children.ts
+++ src/elements/children.ts
@@ -1,6 +1,8 @@
 import {fn} from "../core/core";
 
 fn.prototype.children = () => {
+	if( fn.prototype.shouldDelay(() => {fn.prototype.children.apply(null)}))
+		return fn.prototype;
 	fn.prototype.this = fn.prototype.this[0].children;
 	return fn.prototype;
 }
--- src/elements/replace.ts
+++ src/elements/replace.ts
@@ -1,6 +1,8 @@
 import {fn} from "../core/core";
 
 fn.prototype.replace = (html: string) => {
+	if( fn.prototype.shouldDelay(() => {fn.prototype.replace.apply(null, html)}))
+		return fn.prototype;
 	fn.prototype.this.forEach(element => {
 		element.outerHTML = html;
 	});
