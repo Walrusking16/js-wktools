cbc9c87bec20510c25f9c6669f95234f903eb5e8
Walrusking <walruskinggaming@gmail.com>
2020-09-12 04:02:09 -0400

    Elements: Updated children,clone,empty,replace

--- src/elements/empty.ts
+++ src/elements/empty.ts
@@ -1,6 +1,8 @@
 import {fn} from "../core/core";
 
 fn.prototype.empty = () => {
+	if( fn.prototype.shouldDelay(() => {fn.prototype.empty.apply(null)}))
+		return fn.prototype;
 	fn.prototype.this.forEach(element => {
 		while(element.firstChild)
 			element.removeChild(element.firstChild);
