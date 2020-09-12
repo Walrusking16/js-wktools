32d4d027e545040adbf9c44d6c310da32651bea4
Walrusking <walruskinggaming@gmail.com>
2020-09-12 01:59:38 -0400

    Style: Updated show/hide to be more efficient

--- src/style/hide.ts
+++ src/style/hide.ts
@@ -4,7 +4,6 @@ fn.prototype.hide = () => {
 	if( fn.prototype.shouldDelay(() => {fn.prototype.hide.apply(null)}))
 		return fn.prototype;
 	fn.prototype.this.forEach((element) => {
-		element.setAttribute("current-display", element.style.display);
 		element.style.display = "none";
 	});
 	return fn.prototype;
--- src/style/show.ts
+++ src/style/show.ts
@@ -4,14 +4,7 @@ fn.prototype.show = () => {
 	if( fn.prototype.shouldDelay(() => {fn.prototype.show.apply(null)}))
 		return fn.prototype;
 	fn.prototype.this.forEach((element) => {
-		if(element.style.display !== "none") return;
-		// @ts-ignore
-		if(element.getAttribute("current-display") !== null){
-			element.style.display = element.getAttribute("current-display");
-			element.removeAttribute("current-display");
-		}
-		else
-			element.style.display = "block";
+		element.style.display = "";
 	});
 	return fn.prototype;
 }
