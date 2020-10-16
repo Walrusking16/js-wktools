667ad0ec4a30295aff91a1fd1abb42773c5183ec
Walrusking16 <walruskinggaming@gmail.com>
2020-10-16 15:44:21 -0400

    Style: Updated html/text

--- src/style/html.ts
+++ src/style/html.ts
@@ -1,7 +1,7 @@
 import {fn} from "../core/core";
 
 fn.prototype.html = (_html: string) => {
-	if(_html){
+	if(_html && _html.length > 0){
 		if( fn.prototype.shouldDelay(() => {fn.prototype.html.apply(null, _html)}))
 			return fn.prototype;
 		fn.prototype.this[0].innerHTML = _html;
