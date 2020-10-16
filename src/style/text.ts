fdb1b9bf91a4a949f04a1afd9a7b2939de3c2967
Walrusking16 <walruskinggaming@gmail.com>
2020-10-16 15:21:34 -0400

    Style: Fixed html/text from not returning data

--- src/style/text.ts
+++ src/style/text.ts
@@ -1,9 +1,9 @@
 import {fn} from "../core/core";
 
 fn.prototype.text = (_text: string) => {
-	if( fn.prototype.shouldDelay(() => {fn.prototype.text.apply(null, _text)}))
-		return fn.prototype;
 	if(_text){
+		if( fn.prototype.shouldDelay(() => {fn.prototype.text.apply(null, _text)}))
+			return fn.prototype;
 		fn.prototype.this[0].innerText = _text;
 		return fn.prototype;
 	} else {
