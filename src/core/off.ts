531ebf7e12d87e01c58595609876e3dd46b26e1a
Walrusking16 <walruskinggaming@gmail.com>
2020-10-16 03:18:42 -0400

    Core: Update on/off

--- src/core/off.ts
+++ src/core/off.ts
@@ -4,7 +4,16 @@ fn.prototype.off = (eventName: string) => {
 	if( fn.prototype.shouldDelay(() => {fn.prototype.off.apply(null, [eventName])}))
 		return fn.prototype;
 	fn.prototype.this.forEach((element) => {
-		element.removeEventListener(eventName);
+		if (!Array.isArray(element.eventfunc)) {
+			return fn.prototype;
+		}
+
+		if (!Array.isArray(element.eventfunc[eventName])) {
+			return fn.prototype;
+		}
+		element.eventfunc[eventName].forEach((handler) => {
+			element.removeEventListener(eventName, handler);
+		})
 	});
 	return fn.prototype;
 }
--- src/core/on.ts
+++ src/core/on.ts
@@ -4,7 +4,16 @@ fn.prototype.on = (eventName: string, handler: void) => {
 	if( fn.prototype.shouldDelay(() => {fn.prototype.on.apply(null, [eventName, handler])}))
 		return fn.prototype;
 	fn.prototype.this.forEach((element) => {
+		if (!Array.isArray(element.eventfunc)) {
+			element.eventfunc = [];
+		}
+
+		if (!Array.isArray(element.eventfunc[eventName])) {
+			element.eventfunc[eventName] = [];
+		}
+
 		element.addEventListener(eventName, handler);
+		element.eventfunc[eventName].push(handler);
 	});
 	return fn.prototype;
 }
