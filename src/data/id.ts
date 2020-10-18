dd6c46b8bdefaac0bca8aac1b9266cc86c03c480
Walrusking16 <walruskinggaming@gmail.com>
2020-10-17 21:14:05 -0400

    Data: Added id

---
+++ src/data/id.ts
@@ -0,0 +1,11 @@
+import {fn} from "../core/core";
+
+fn.prototype.id = (arg) => {
+	if( fn.prototype.shouldDelay(() => {fn.prototype.id.apply(null, arg)}))
+		return fn.prototype;
+	if (arg === null)
+		return fn.prototype.this[0].id;
+	else
+		fn.prototype.this[0].id = arg;
+	return fn.prototype;
+}
