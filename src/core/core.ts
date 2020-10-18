2b9d154f984f2990b32f2882f11385d49c6a5d49
Walrusking16 <walruskinggaming@gmail.com>
2020-10-17 21:14:22 -0400

    Core: Added selector

--- src/core/core.ts
+++ src/core/core.ts
@@ -1,16 +1,23 @@
 window['$'] = function(...args) {
 	if (typeof args[0] === 'function') {
 		document.addEventListener('DOMContentLoaded', args[0]);
-	} else if (typeof args[0] === 'string') {
-		const _collection = document.querySelectorAll(args[0]);
-		return fn.apply(_collection);
-	} else if (args[0] instanceof HTMLElement) {
-		const _collection = [args[0]];
-		return fn.apply(_collection);
+	} else if (typeof args[0] === 'string' || args[0] instanceof HTMLElement) {
+		return fn.apply(cash.selector(args[0]));
 	}
 }
 
 export const cash = window['$'];
+
+cash.selector = function(select){
+	let results = null;
+	if(typeof select === 'string'){
+		results = document.querySelectorAll(select);
+	}
+	else if (select instanceof HTMLElement) {
+		results = [select];
+	}
+	return results;
+}
 
 export const fn = function(){
 	fn.prototype.queue = [];
