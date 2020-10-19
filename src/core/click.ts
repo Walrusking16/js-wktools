3bddd3eab132b48f2b9f8e7cc3ab6f8e5677ad52
Walrusking16 <walruskinggaming@gmail.com>
2020-10-19 17:51:58 -0400

    Core: Fixed bug in click

--- src/core/click.ts
+++ src/core/click.ts
@@ -1,7 +1,7 @@
 import {fn} from "./core";
 
 fn.prototype.click = (handler: Function, preventDefault: boolean = false) => {
-	if( fn.prototype.shouldDelay(() => {fn.prototype.click.apply(null, [handler])}))
+	if( fn.prototype.shouldDelay(() => {fn.prototype.click.apply(null, [handler, preventDefault])}))
 		return fn.prototype;
 
 	fn.prototype.on("click", (event) => {
