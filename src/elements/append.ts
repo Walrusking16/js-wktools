476fd242a6f29fb4bd06f5296e756f8a0f2d0fc0
Walrusking16 <walruskinggaming@gmail.com>
2020-10-23 12:10:30 -0400

    Elements: Updated append

--- src/elements/append.ts
+++ src/elements/append.ts
@@ -1,10 +1,8 @@
 import {fn} from "../core/core";
 
 fn.prototype.append = (element) => {
-	if( fn.prototype.shouldDelay(() => {fn.prototype.append.apply(null, element)}))
+	if( fn.prototype.shouldDelay(() => {fn.prototype.append.apply(null, [element])}))
 		return fn.prototype;
-	fn.prototype.this.forEach((elem)=>{
-		elem.appendChild(element);
-	});
-	return null;
+	fn.prototype.this[0].appendChild(element);
+	return fn.prototype;
 }
