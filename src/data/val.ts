22790f4e67b9b9eeb7313041ad4c7ed0fbde7352
Walrusking16 <walruskinggaming@gmail.com>
2020-10-23 12:11:42 -0400

    Data: Fixed val setting value to undefined

--- src/data/val.ts
+++ src/data/val.ts
@@ -3,7 +3,7 @@ import {fn} from "../core/core";
 fn.prototype.val = (arg) => {
 	if( fn.prototype.shouldDelay(() => {fn.prototype.val.apply(null, arg)}))
 		return fn.prototype;
-	if (arg === null)
+	if (arg === undefined)
 		return fn.prototype.this[0].value;
 	else
 		fn.prototype.this[0].value = arg;
