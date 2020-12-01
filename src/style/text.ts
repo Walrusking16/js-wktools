0556b909786444917878dd6f8f0e8089d782cd95
Walrusking16 <walruskinggaming@gmail.com>
2020-11-30 22:58:00 -0500

    Style: Updated Text

--- src/style/text.ts
+++ src/style/text.ts
@@ -1,7 +1,7 @@
 import {fn} from "../core/core";
 
 fn.prototype.text = (_text: string) => {
-	if(_text && _text.length > 0){
+	if(_text && _text.length > 0 || _text == ""){
 		if( fn.prototype.shouldDelay(() => {fn.prototype.text.apply(null, _text)}))
 			return fn.prototype;
 		fn.prototype.this[0].innerText = _text;
